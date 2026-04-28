0703bcff6b5f050fe0712d7e493249054949fbcf
AdamKuzniarski <adam.kuzniarski@gmx.de>
2026-04-28 11:39:58 +0200

    extend TunnelFocusControlModule.ts with 2 Methods applyShield and clearShield

--- apps/mobile/modules/tunnel-focus-control/src/TunnelFocusControlModule.ts
+++ apps/mobile/modules/tunnel-focus-control/src/TunnelFocusControlModule.ts
@@ -3,11 +3,14 @@ import { NativeModule, requireNativeModule } from 'expo';
 import {
   TunnelAuthorizationStatus,
   TunnelFocusControlModuleEvents,
+  TunnelShieldResult,
 } from './TunnelFocusControl.types';
 
 declare class TunnelFocusControlModule extends NativeModule<TunnelFocusControlModuleEvents> {
   getAuthorizationStatus(): Promise<TunnelAuthorizationStatus>;
   requestAuthorization(): Promise<TunnelAuthorizationStatus>;
+  applyShield(): Promise<TunnelShieldResult>;
+  clearShield(): Promise<TunnelShieldResult>;
 }
 
 export default requireNativeModule<TunnelFocusControlModule>('TunnelFocusControl');
