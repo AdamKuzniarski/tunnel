5429d54a02eb13cfd9ccecd2fd5ccb0e60da6809
AdamKuzniarski <adam.kuzniarski@gmx.de>
2026-05-17 13:48:02 +0200

    expand the interface with getSelectionSummary, and clearSelection

--- apps/mobile/modules/tunnel-focus-control/src/TunnelFocusControlModule.ts
+++ apps/mobile/modules/tunnel-focus-control/src/TunnelFocusControlModule.ts
@@ -3,6 +3,7 @@ import { NativeModule, requireNativeModule } from 'expo';
 import {
   TunnelAuthorizationStatus,
   TunnelFocusControlModuleEvents,
+  TunnelSelectionSummary,
   TunnelShieldResult,
 } from './TunnelFocusControl.types';
 
@@ -11,6 +12,8 @@ declare class TunnelFocusControlModule extends NativeModule<TunnelFocusControlMo
   requestAuthorization(): Promise<TunnelAuthorizationStatus>;
   applyShield(): Promise<TunnelShieldResult>;
   clearShield(): Promise<TunnelShieldResult>;
+  getSelectionSummary(): Promise<TunnelSelectionSummary>;
+  clearSelection(): Promise<TunnelSelectionSummary>;
 }
 
 export default requireNativeModule<TunnelFocusControlModule>('TunnelFocusControl');
