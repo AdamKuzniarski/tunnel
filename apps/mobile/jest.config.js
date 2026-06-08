1630f4b24c3313cab6d03345493ba893c49b469e
AdamKuzniarski <adam.kuzniarski@gmx.de>
2026-06-08 14:28:41 +0200

    update jest.config.js for ci

--- apps/mobile/jest.config.js
+++ apps/mobile/jest.config.js
@@ -23,5 +23,13 @@ module.exports = {
     '/.expo/',
     '/dist/',
   ],
+  coverageThreshold: {
+    global: {
+      statements: 70,
+      branches: 60,
+      functions: 70,
+      lines: 70,
+    },
+  },
 };
 
